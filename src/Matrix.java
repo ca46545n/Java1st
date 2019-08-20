cb52b3b8952577e7e0a90fe9c9cbef5e01ba190c
calegria <ca46545n@pace.edu>
2019-08-20 15:10:57 -0400

    5

---
+++ src/Matrix.java
@@ -0,0 +1,37 @@
+
+
+public class Matrix {
+
+
+    public static void main (String[] args) {
+
+
+        int[][] Matrix = {{30, 15, 10},
+                         { 8 , 7 , 19},
+                         { 6,  12 , 50}
+
+
+
+
+
+        };
+
+
+
+        for(int i = 0; i < 3; i++){
+            for (int c = 0; c < 3; c++){
+
+                System.out.println(Matrix[i][c]);
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+
+}
